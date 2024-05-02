bb4c9999eeee609c2d57afb3075795f12221fed3
phanquangduong2002 <phanquangduong2002@gmail.com>
2024-05-03 00:21:43 +0700

    commit

--- client/src/stores/modules/homeStore.js
+++ client/src/stores/modules/homeStore.js
@@ -1,16 +1,20 @@
 import { defineStore } from 'pinia'
 import { HOME_STORE } from './../../configs/storeTypes'
 
-export const useHomeStore = defineStore(
-  HOME_STORE,
-  {
-    state: () => ({}),
-    actions: {}
-  },
-  {
-    persist: true
+export const useHomeStore = defineStore(HOME_STORE, {
+  state: () => ({
+    conversations: [],
+    selectedConversation: null,
+    localConversations: [],
+    sortedConversations: [],
+    onlineUsers: {}
+  }),
+  actions: {
+    isUserOnline(userId) {
+      return this.onlineUsers[userId]
+    }
   }
-)
+})
 
 export default {
   useHomeStore
